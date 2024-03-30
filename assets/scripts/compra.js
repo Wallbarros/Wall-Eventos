81cc53eda379b8f857935a7e3ce11f50aba09b5a
Wallace <139662938+Wallbarros@users.noreply.github.com>
2024-03-29 23:17:18 -0300

    atualizações

---
+++ assets/scripts/compra.js
@@ -0,0 +1,17 @@
+//Utilizar esse comando para recuperar os dados na segunda
+const shows = [
+    {img: '../assets/images/53410916675_363215546c_h.jpg', descrição: 'aaaaa'},
+    {img: '../assets/images/53427063651_23ff2e2789_h.jpg', descrição: 'bbbbbbb'}
+]
+
+
+
+const button = document.getElementById('button-compra')
+button.addEventListener('click', (e) => {
+    alert('aaaaaaaaa')
+})
+const imagem = document.getElementById('show-img')
+    imagem.addEventListener('click', (e) => {
+    alert('aaaaaaaaaa')
+})
+
---
+++ assets/scripts/script.js
@@ -0,0 +1,12 @@
+const shows = [
+    {img: '../assets/images/53410916675_363215546c_h.jpg', descrição: 'aaaaa'},
+    {img: '../assets/images/53427063651_23ff2e2789_h.jpg', descrição: 'bbbbbbb'}
+]
+
+
+function clicou(id) {
+    let showId = id    
+    return showId
+}
+
+let id = c
